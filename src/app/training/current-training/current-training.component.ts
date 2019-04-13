b4e569a6fd3b29b8fbb0d48f167f1832bc3fa181
Vladimir Tambovtsev <tambovcev99@mail.ru>
2019-04-13 16:24:58 +0300

    training page tabs & timer

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  progress = 0;
+  phrase = 'You can do it, keep going!';
+  timer: number;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.timer = setInterval(() => {
+      this.progress = this.progress + 20;
+      if (this.progress >= 100) {
+        clearInterval(this.timer);
+      } else if (this.progress >= 70) {
+        // tslint:disable-next-line: quotemark
+        this.phrase = "Don't give up, almost done!";
+      }
+    }, 1000);
   }
 
+  onStop() {
+    clearInterval(this.timer);
+  }
 }
