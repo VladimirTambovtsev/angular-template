bf9c54dfdbff41fab7d00b251a24bff5f5fbd8b9
Vladimir Tambovtsev <tambovcev99@mail.ru>
2019-04-13 17:49:26 +0300

    auth subscription on nav components finished

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,18 +1,23 @@
 import { Subject } from 'rxjs/Subject';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { AuthData } from './auth-data.model';
 import { User } from './user.model';
 
+@Injectable()
 export class AuthService {
   authChange = new Subject<boolean>();
   private user: User;
+
+  constructor(private router: Router) {}
 
   registerUser(authData: AuthData) {
     this.user = {
       email: authData.email,
       userId: Math.round(Math.random() * 10000).toString()
     };
-    this.authChange.next(true);
+    this.authSucces();
   }
 
   login(authData: AuthData) {
@@ -20,12 +25,18 @@ export class AuthService {
       email: authData.email,
       userId: Math.round(Math.random() * 10000).toString()
     };
+    this.authSucces();
+  }
+
+  authSucces() {
     this.authChange.next(true);
+    this.router.navigate(['/training']);
   }
 
   logout() {
     this.user = null;
     this.authChange.next(false);
+    this.router.navigate(['/login']);
   }
 
   getUser() {
